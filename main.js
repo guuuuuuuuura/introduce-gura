2d8d4792cb880a003b2ac73d1d68fea1ebfcaadd
gura <guraytch3413@gmail.com>
2020-08-13 15:20:30 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+$(function(){
+    var backimg = $('#backimg');
+        backimg.hide().fadeIn(1300,'easeInQuad')
+
+});
+
+$(function(){
+    var effect_pos = 180; // 画面下からどの位置でフェードさせるか(px)
+    var effect_move = 50; // どのぐらい要素を動かすか(px)
+    var effect_time = 800; // エフェクトの時間(ms) 1秒なら1000
+
+    // フェードする前のcssを定義
+    $('.imgwrap').css({
+        opacity: 0,
+        transform: 'translateY('+ effect_move +'px)',
+        transition: effect_time + 'ms'
+    });
+
+    // スクロールまたはロードするたびに実行
+    $(window).on('scroll load', function(){
+        var scroll_top = $(this).scrollTop();
+        var scroll_btm = scroll_top + $(this).height();
+        effect_pos = scroll_btm - effect_pos;
+
+        // effect_posがthis_posを超えたとき、エフェクトが発動
+        $('.imgwrap').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( effect_pos > this_pos ) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+            }
+        });
+    });
+});
+
+$(function(){
+
+    var effect_btm = 300; // 画面下からどの位置でフェードさせるか(px)
+    var effect_move = 50; // どのぐらい要素を動かすか(px)
+    var effect_time = 900; // エフェクトの時間(ms) 1秒なら1000
+
+    //親要素と子要素のcssを定義
+    $('.card').css({
+        opacity: 0
+    });
+    $('.card').children().each(function(){
+        $(this).css({
+            opacity: 0,
+            transform: 'translateY('+ effect_move +'px)',
+            transition: effect_time + 'ms'
+        });
+    });
+
+    // スクロールまたはロードするたびに実行
+    $(window).on('scroll load', function(){
+        var scroll_top = $(this).scrollTop();
+        var scroll_btm = scroll_top + $(this).height();
+        var effect_pos = scroll_btm - effect_btm;
+
+        //エフェクトが発動したとき、子要素をずらしてフェードさせる
+        $('.card').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( effect_pos > this_pos ) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+                $(this).children().each(function(i){
+                    $(this).delay(100 + i*200).queue(function(){
+                        $(this).css({
+                            opacity: 1,
+                            transform: 'translateY(0)'
+                        }).dequeue();
+                    });
+                });
+            }
+        });
+    });
+
+});
+
